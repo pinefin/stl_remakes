8fc2ed2c785be90d5b785412bbcef1664d8c6a7f
pinefin <45133527+pinefin@users.noreply.github.com>
2024-03-10 16:20:14 -0500

    Update crt_remakes.h

--- crt_remakes/crt_remakes.h
+++ crt_remakes/crt_remakes.h
@@ -1,21 +1,25 @@
 #pragma once
 
-typedef __SIZE_TYPE__ size_t;
+#if _WIN32
+#include <Windows.h>
+#endif
 
 #ifndef NULL
 #define NULL (0)
 #endif
 
-size_t memset(char *s, int c, size_t n);
+void* memset(void *s, int c, size_t n);
 
 void *memmove(void *dest, const void *src, size_t n);
 
 size_t strlen(const char *s);
 
-void memcpy(char *_dest, const char *_src, size_t len);
+void* memcpy(void *_dest, void*_src, size_t len);
 
 int memcmp(const void *s1, const void *s2, size_t n);
 
 int strcmp(const char *s1, const char *s2);
 
-char *strstr(const char *str, const char *substring);
+#if _WIN32
+char* strstr(char* const str, const char* const substring);
+#endif
